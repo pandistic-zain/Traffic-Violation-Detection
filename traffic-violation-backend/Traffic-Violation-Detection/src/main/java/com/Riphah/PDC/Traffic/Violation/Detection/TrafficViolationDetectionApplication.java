7786ff180dfd6f6d70df3ffb8bd540d61eb5a2bc
Zain Ul Abdeen <zain.alphanetworks@gmail.com>
2024-10-30 10:05:25 +0500

    commit

--- traffic-violation-backend/Traffic-Violation-Detection/src/main/java/com/Riphah/PDC/Traffic/Violation/Detection/TrafficViolationDetectionApplication.java
+++ traffic-violation-backend/Traffic-Violation-Detection/src/main/java/com/Riphah/PDC/Traffic/Violation/Detection/TrafficViolationDetectionApplication.java
@@ -2,8 +2,10 @@ package com.Riphah.PDC.Traffic.Violation.Detection;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
 
 @SpringBootApplication
+@EnableMongoRepositories(basePackages = "com.Riphah.PDC.Traffic.Violation.Detection.Repository")
 public class TrafficViolationDetectionApplication {
 
 	public static void main(String[] args) {
