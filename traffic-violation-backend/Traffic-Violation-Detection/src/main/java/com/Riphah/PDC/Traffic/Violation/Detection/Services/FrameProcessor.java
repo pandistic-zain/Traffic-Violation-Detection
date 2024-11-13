e28cbddaca7eefd94c2534392518920d0d1b401b
Zain Ul Abdeen <zain.alphanetworks@gmail.com>
2024-11-14 01:24:26 +0500

    "Updated Flask URL in FrameProcessor from https://56db-35-193-0-52.ngrok-free.app/analyze to https://8e49-35-245-69-118.ngrok-free.app/analyze"

--- traffic-violation-backend/Traffic-Violation-Detection/src/main/java/com/Riphah/PDC/Traffic/Violation/Detection/Services/FrameProcessor.java
+++ traffic-violation-backend/Traffic-Violation-Detection/src/main/java/com/Riphah/PDC/Traffic/Violation/Detection/Services/FrameProcessor.java
@@ -59,7 +59,7 @@ public class FrameProcessor implements Callable<List<String>> {
             });
 
             HttpEntity<MultiValueMap<String, Object>> requestEntity = new HttpEntity<>(body, headers);
-            String flaskUrl = "https://56db-35-193-0-52.ngrok-free.app/analyze";
+            String flaskUrl = "https://8e49-35-245-69-118.ngrok-free.app/analyze";
             System.out.println("Sending request to Flask at URL: " + flaskUrl);
 
             ResponseEntity<byte[]> response = restTemplate.postForEntity(flaskUrl, requestEntity, byte[].class);
